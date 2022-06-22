16efacff9c89dc5e0094d0ae441facb8380e1954
Eduarda <85466761+EduardaMatias@users.noreply.github.com>
2022-06-22 16:58:12 -0300

    adicionando enunciado do exercício2

--- MainData.java
+++ MainData.java
@@ -1,3 +1,8 @@
+/**
+ * Eduarda Vitória e Stefany Tam
+ 
+ * Agora, desenvolva um programa capaz de testar a classe e os métodos desenvolvidos no exercício anterior.
+ */
 
 public class MainData {
 	public static void main(String[] args) {
